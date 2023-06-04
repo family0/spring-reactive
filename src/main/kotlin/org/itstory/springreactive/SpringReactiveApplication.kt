661f9bde1b146ac1cf87fa0c49adec0c6ed46865
TJ <mail@itstory.org>
2023-06-04 20:22:51 +0900

    Spring init

---
+++ src/main/kotlin/org/itstory/springreactive/SpringReactiveApplication.kt
@@ -0,0 +1,11 @@
+package org.itstory.springreactive
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringReactiveApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringReactiveApplication>(*args)
+}
---
+++ src/test/kotlin/org/itstory/springreactive/SpringReactiveApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.itstory.springreactive
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
